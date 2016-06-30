3389d3ace336ef4feafc5a20c8ff5e54122711cd
Frankdew <frankdew23@gmail.com>
2016-06-30 14:39:37 +0200

    Show post user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :posts
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  def handle
+    self.email.split('@')[0]
+  end
 end
