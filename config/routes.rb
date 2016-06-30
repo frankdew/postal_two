cab8b5b8a0b18975a48189eea988a3480818a264
Frankdew <frankdew23@gmail.com>
2016-06-30 14:57:47 +0200

    Like posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :posts
 
-  get 'user_posts/:user_id' => 'posts#user', as: :user_posts
+  resources :posts  do
+    resources :likes
+  end
 end
